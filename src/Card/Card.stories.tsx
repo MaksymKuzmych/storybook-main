efa17c0e50ea540dea95b5112fd960a4dd2154d6
Maksym Kuzmych <kuzmichmaksym1997@gmailc.om>
2023-11-02 12:32:47 +0200

    qwe

---
+++ src/Card/Card.stories.tsx
@@ -0,0 +1,47 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Card } from './Card';
+
+const meta = {
+  title: 'UI/Cards/Card',
+  component: Card,
+  parameters: {
+    layout: 'centered',
+    backgrounds: {
+      values: [
+        { name: 'red', value: 'red' },
+        { name: 'dark', value: '#050449' },
+      ],
+    },
+  },
+  tags: ['autodocs'],
+  argTypes: {
+    backgroundColor: { control: 'color', description: 'Background color' },
+    padding: {
+      control: {
+        type: 'select',
+        options: ['10px', '20px', '30px'],
+      },
+      description: 'Padding',
+    },
+  },
+  decorators: [
+    (Story) => (
+      <div style={{ width: '100%', maxWidth: '500px' }}>
+        <Story />
+      </div>
+    ),
+  ],
+} satisfies Meta<typeof Card>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {
+    title: 'My Card',
+    backgroundColor: 'red',
+    padding: '10px',
+  },
+};
---
+++ src/Card/Card.tsx
@@ -0,0 +1,9 @@
+interface ICardProps {
+  title: string;
+  backgroundColor: string;
+  padding: '10px' | '20px' | '30px';
+}
+
+export const Card = ({ title, backgroundColor, padding }: ICardProps) => {
+  return <div style={{ backgroundColor, padding }}>{title}</div>;
+};
